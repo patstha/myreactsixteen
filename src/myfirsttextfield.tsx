fb589657354478ccdd77d220c6ed77c3674a5504
pat <pat@tilde.team>
2023-04-28 13:29:31 -0400

    build

--- src/myfirsttextfield.tsx
+++ src/myfirsttextfield.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { TextField } from 'office-ui-fabric-react';
 
 export const MyFirstTextField: React.FunctionComponent = () => {
-    let mytextfield = "";
     return (
         <>
             <TextField
                 title="my title"
                 description="my description"
-                value={mytextfield}
                 placeholder="my placeholder"
             >
             </TextField>
