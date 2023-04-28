2e6956c0a1ebb7874345c763d4d1a9a0dc3d9de8
pat <pat@tilde.team>
2023-04-28 12:51:09 -0400

    build

--- src/program.tsx
+++ src/program.tsx
@@ -2,7 +2,7 @@ import React from "react";
 export const Program: React.FunctionComponent = () => {
     return (
         <>
-        <h1>Hello, Kushal!</h1>
+        <h1>Hello, Pratikchhya!</h1>
         </>
     );
 }
